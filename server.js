6f041e5c77cc82ba51f2a3e842abf965ae9bd362
Roman Kasko <mailto.32roman@gmail.com>
2022-12-18 20:40:10 +0200

    middleware added

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
-app.search(express)
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.search(express.json());
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
@@ -18,5 +29,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
